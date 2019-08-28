5861c41358c273ac77a83423510cf0e6ee93d06d
haim19 <hai.nahum@gmail.com>
2019-08-28 16:45:19 +0300

    chnges on server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,115 @@ var router = express.Router();
 var CustomerModel = require('../models/customers.model');
 var ProductModel = require('../models/product.model');
 var CategoryModel = require('../models/category.model');
+var CartModel = require('../models/cart.model');
+var CartProductModel = require('../models/cartProduct.model');
+var OrderModel = require('../models/order.model');
 
+router.post('/get_cart_product', function (req, res) {
+  let products = [];
+  let lastProduct = req.body.prodList[req.body.prodList.length - 1];
+  req.body.prodList.forEach(prod => {
+    ProductModel.findOne({ _id: prod }).then(data => {
+      products.push(data);
+      if (prod === lastProduct) {
+        res.json(products);
+      }
+    })
+  });
+})
 
+router.get('/logout', async function (req, res) {
+  try {
+    req.session.destroy();
+    res.json({ msg: "destoyed" });
+  }
+  catch (error) {
+    console.log(error);
+  }
+})
+
+router.post('/cart', function (req, res) {
+  cartModel = new CartModel({
+    customer: req.body.customer,
+    creationDate: new Date()
+  })
+  CartModel.findOne({ customer: req.body.customer }).then(data => {
+    if (data) {
+      res.json(data._id);
+    }
+    else {
+      cartModel.save((err, doc) => {
+        if (err) throw err;
+        res.json(doc);
+      })
+    }
+  })
+})
+router.post('/search_cart', function (req, res) {
+  CartModel.findOne({ customer: req.body.customer }).then(data => {
+    res.json(data);
+  })
+})
+router.post('/search_order', function (req, res) {
+  OrderModel.find({ customerId: req.body.customer }).then(data => {
+    res.json(data);
+  })
+})
+
+router.post('/search_user_products', function (req, res) {
+  CartProductModel.find({ cartId: req.body.cartId }).then(data => {
+    res.json(data);
+  })
+})
+router.put('/cart_product', function (req, res) {
+
+  CartProductModel.updateMany({ cartId: req.body.cartId },
+    {
+      $set: {
+        cartId: req.body.cartId + '_LOCKED_' + req.body.generated
+      }
+    },
+    {
+      new: true
+    },
+    function (error, updatedDoc) {
+      if (error) throw error;
+      res.json(updatedDoc);
+    }
+  )
+})
+router.put('/cart', function (req, res) {
+
+  CartModel.updateMany({ customer: req.body.customer },
+    {
+      $set: {
+        customer: req.body.customer + '_LOCKED_' + req.body.generated
+      }
+    },
+    {
+      new: true
+    },
+    function (error, updatedDoc) {
+      if (error) throw error;
+      res.json(updatedDoc);
+    }
+  )
+})
+
+router.post('/cart_product', function (req, res) {
+  cartProductModel = new CartProductModel({
+    ProdId: req.body.ProdId,
+    cartId: req.body.cartId,
+    amount: req.body.amount,
+    totalPrice: req.body.totalPrice
+
+  })
+  cartProductModel.save((err, doc) => {
+    if (err) throw err;
+    res.json(doc);
+
+  })
+})
 
 router.get('/prelog', async (req, res) => {
   try {
@@ -68,6 +175,48 @@ router.post('/login', async (req, res) => {
   res.json(data);
 
 })
+router.delete('/product/:id', async (req, res) => {
+  try {
+    var deleted = await CartProductModel.remove({
+      _id: req.params.id,
+      cartId: req.query.cartId
+    })
+    await res.json(deleted);
+  }
+  catch (error) {
+    console.log(error);
+  }
+})
+
+router.delete('/products/:id', async (req, res) => {
+  try {
+    let deletedProduct = await CartProductModel.deleteMany({ cartId: req.params.id })
+    let deletedCart = await CartModel.deleteOne({ _id: req.params.id })
+    await res.json({ deletedProduct: deletedProduct, deletedCart: deletedCart });
+  }
+  catch (error) {
+    console.log(error);
+  }
+})
+router.post('/order', function (req, res) {
+  orderModel = new OrderModel({
+    customerId: req.body.customerId,
+    cartId: req.body.cartId,
+    finalPrice: req.body.finalPrice,
+    scheduled: req.body.scheduled,
+    city: req.body.city,
+    street: req.body.street,
+    generated: req.body.generated,
+    card: req.body.card
+
+  })
+  orderModel.save((err, doc) => {
+    if (err) throw err;
+    res.json(doc);
+
+  })
+})
+
 
 router.post('/checkid', async (req, res) => {
 
@@ -88,5 +237,25 @@ router.post('/checkid', async (req, res) => {
   }
 })
 
+router.put('/product', function (req, res) {
+
+  ProductModel.findByIdAndUpdate({ _id:req.body.prodId },
+    {
+      $set: {
+        title:req.body.title,
+        url:req.body.url,
+        price:req.body.price,
+        categoryType:req.body.categoryType
+      }
+    },
+    {
+      new: true
+    },
+    function (error, updatedDoc) {
+      if (error) throw error;
+      res.json(updatedDoc);
+    }
+  )
+})
 
 module.exports = router;
